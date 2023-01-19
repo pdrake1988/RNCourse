c276e60676e5fbeefec86a0229f25132c7852996
pdrake1988 <pdrake1@me.com>
2023-01-18 19:50:53 -0600

    initial

---
+++ app.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.box}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    borderColor: "red",
+    margin: 5,
+    borderWidth: 20,,
+  },
+});
